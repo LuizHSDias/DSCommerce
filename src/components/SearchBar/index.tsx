60ea41e72daa71df30da318efdf3bf923b12f2ab
LHSDias <luizhenriquemc15@gmail.com>
2026-02-23 19:47:38 -0300

    Props

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+function SearchBar(){
+    return (
+        <>
+         <form className="dsc-search-bar">
+          <button type="submit">🔎︎</button>
+          <input type="text" placeholder="Nome do produto" />
+          <button type="reset">🗙</button>
+        </form>
+        </>
+    );
+}
+
+export default SearchBar
---
+++ src/routes/ProductDetails/index.tsx
@@ -0,0 +1,47 @@
+import './styles.css'
+import ButtonInverse from "../../components/ButtonInverse";
+import ButtonPrimary from "../../components/ButtonPrimary";
+import HeaderClient from "../../components/HeaderClient";
+import ProductDetailsCard from "../../components/ProductDetailsCard";
+import type { ProductDTO } from '../../models/product';
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Essa TV é Top",
+  imgUrl: "https://github.dev/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos"
+    }, 
+    {
+      id: 3,
+      name: "Computadores"
+    },
+    {
+      id: 4,
+      name: "Importados"
+    }
+  ]
+}
+
+function ProductDetails (){
+    return (
+        <>
+    <HeaderClient />
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        <ProductDetailsCard product={product} />
+        <div className="dsc-btn-page-container">
+            <ButtonPrimary />
+            <ButtonInverse />
+        </div>
+      </section>
+    </main>
+    </>
+    );
+}
+
+export default ProductDetails
