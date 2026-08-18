17dce38a48969ea96155bff0d05f84a59a9c701e
LHSDias <luizhenriquemc15@gmail.com>
2026-08-18 17:54:17 -0300

    Corrigindo o problema do subTotal e total

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import { useState } from "react";
 import * as cartService from "../../../services/cart-service";
 import { OrderDTO } from "../../../models/order";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
   const [cart, setCart] = useState<OrderDTO>(cartService.getCart);
@@ -11,7 +12,9 @@ export default function Cart() {
       <section id="cart-container-section" className="dsc-container">
         {cart.items.length === 0 ? (
           <div>
-            <h2 className="dsc-section-title dsc-mb20">Seu Carrinho está Vazio</h2>
+            <h2 className="dsc-section-title dsc-mb20">
+              Seu Carrinho está Vazio
+            </h2>
           </div>
         ) : (
           <div className="dsc-card dsc-mb20">
@@ -45,7 +48,9 @@ export default function Cart() {
 
         <div className="dsc-btn-page-container">
           <div className="dsc-btn dsc-btn-blue">Finalizar pedido</div>
-          <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
+          <Link to="/catalog">
+            <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
+          </Link>
         </div>
       </section>
     </main>
